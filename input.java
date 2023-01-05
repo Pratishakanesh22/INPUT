8cb218a8da6b0e39d20bc078f4ae3ff326ad8989
pratisha kanesh <91782860+Pratishakanesh22@users.noreply.github.com>
2023-01-05 23:44:42 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class input {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+String name = sc.next();
+Double d = sc.nextDouble();
+int number = sc.nextInt();
+
+System.out.println(" String: "+ name );
+System.out.println(" Double: "+ d);
+System.out.println(" Int: "+ number );
+    }
+}
